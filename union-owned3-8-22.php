e9685a5b97159325ffeefd3702fe768d319da968
Dillon Ellis <dellis@fcul.com>
2022-05-04 10:37:41 -0400

    repo car for sale

--- union-owned3-8-22.php
+++ union-owned3-8-22.php
@@ -1,87 +1,93 @@
 ﻿<!doctype html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Family First Credit Union | CU Owned Vehicles</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Family First Credit Union | CU Owned Vehicles</title>
 
-<!-- Meta -->
-<meta name="keywords" content="" />
-<meta name="author" content="">
-<meta name="robots" content="" />
-<meta name="description" content="" />
+  <!-- Meta -->
+  <meta name="keywords" content="" />
+  <meta name="author" content="">
+  <meta name="robots" content="" />
+  <meta name="description" content="" />
 
-<!-- Favicon -->
-<link rel="shortcut icon" href="images/logo.png" type="image/x-icon" />
-<link rel="apple-touch-icon" href="images/logo.png">
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="images/logo.png" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="images/logo.png">
 
-<!-- Web Fonts  -->
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800|Roboto:400,300,500,700,900|Raleway:400,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
-<!--[if lt IE 9]>
+  <!-- Web Fonts  -->
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800|Roboto:400,300,500,700,900|Raleway:400,300,500,600,700,800,900' rel='stylesheet' type='text/css'>
+  <!--[if lt IE 9]>
 <script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 
-<!-- stylesheets -->
-<link rel="stylesheet" media="screen" href="js/bootstrap/css/bootstrap.min.css" type="text/css" />
-<link rel="stylesheet" href="js/mainmenu/menu-5.css" type="text/css" />
-<link rel="stylesheet" href="css/theme-default.css" type="text/css" />
-<link rel="stylesheet" href="css/theme-style.css" type="text/css" />
-<link rel="stylesheet" href="css/shortcodes.css" type="text/css" />
-<link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
-<link rel="stylesheet" href="js/masterslider/style/masterslider.css" />
-<link href="js/animations/css/animations.min.css" rel="stylesheet" type="text/css" media="all" />
-<link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
-<link rel="stylesheet" media="screen" href="css/responsive-style.css" type="text/css" />
-<link rel="stylesheet" type="text/css" href="js/tabs/assets/css/responsive-tabs.css">
-<link rel="stylesheet" href="js/ytplayer/ytplayer.css" />
-<script type="text/javascript">
-            function postLink(link) {
-                var f = document.createElement("form");
-                f.method = 'post';
-                f.action = link;
-                document.body.appendChild(f);
-                f.submit();
-            }
-        </script>
+  <!-- stylesheets -->
+  <link rel="stylesheet" media="screen" href="js/bootstrap/css/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="js/mainmenu/menu-5.css" type="text/css" />
+  <link rel="stylesheet" href="css/theme-default.css" type="text/css" />
+  <link rel="stylesheet" href="css/theme-style.css" type="text/css" />
+  <link rel="stylesheet" href="css/shortcodes.css" type="text/css" />
+  <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="js/masterslider/style/masterslider.css" />
+  <link href="js/animations/css/animations.min.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link rel="stylesheet" media="screen" href="css/responsive-style.css" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="js/tabs/assets/css/responsive-tabs.css">
+  <link rel="stylesheet" href="js/ytplayer/ytplayer.css" />
+  <script type="text/javascript">
+    function postLink(link) {
+      var f = document.createElement("form");
+      f.method = 'post';
+      f.action = link;
+      document.body.appendChild(f);
+      f.submit();
+    }
+  </script>
 </head>
 
 <body>
-<div class="site_wrapper font-style5">
-	
-<!--************THIS IS WHERE WE ARE INCLUDING THE HEADER!!!! ************
+  <div class="site_wrapper font-style5">
+
+    <!--************THIS IS WHERE WE ARE INCLUDING THE HEADER!!!! ************
 	YOU CAN FIND IT IN HEADER.php!!!!-->
-	
-  <div w3-include-html="header.php"></div>
-	
-<!--*********HEADER HEADER HEADER HEADER HEADER HEADER **********-->	
-<div class="clearfix"></div>
-  
 
-  <div class="titlebar four">
-  <div class="container">
-   <div class="breadcrumb">
-    <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6"><h1>Services</h1></div>
-<div class="col-lg-6 col-md-6 col-sm-6">
-<div class="pagenation"><a href="index.php">Home</a> <i class="fa fa-angle-double-right"></i> <a href="index.php">Pages</a> <i class="fa fa-angle-double-right"></i> CU Owned Vehicles</div></div>
-      </div></div></div>
-      
-   <img src="images/banner4.jpg" class="img-responsive" alt="banner for interior page"> 
-   		
-   </div>
-  
-  <div class="clearfix margin-top8"></div>
-  
-  
-  
+    <div w3-include-html="header.php"></div>
+
+    <!--*********HEADER HEADER HEADER HEADER HEADER HEADER **********-->
+    <div class="clearfix"></div>
 
 
-<div class="container">
-<div class="row">
-<div class="col-lg-3 col-md-3 col-sm-5">
+    <div class="titlebar four">
+      <div class="container">
+        <div class="breadcrumb">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+              <h1>Services</h1>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+              <div class="pagenation"><a href="index.php">Home</a> <i class="fa fa-angle-double-right"></i> <a href="index.php">Pages</a> <i class="fa fa-angle-double-right"></i> CU Owned Vehicles</div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-<!--<div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
+      <img src="images/banner4.jpg" class="img-responsive" alt="banner for interior page">
+
+    </div>
+
+    <div class="clearfix margin-top8"></div>
+
+
+
+
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-5">
+
+          <!--<div class="col-md-12 col-sm-12 col-xs-12 nopadding bmargin">
 <form action="#" class="site_search">
 <input placeholder="Search..." type="text">
 <button type="submit" class="site_search_btn"><i class="fa fa-search"></i></button>
@@ -90,64 +96,73 @@
 
 <div class="clearfix margin-top3"></div>-->
 
-<div w3-include-html="side-nav-services.php"></div>
+          <div w3-include-html="side-nav-services.php"></div>
 
-<div class="clearfix margin-top3"></div>
+          <div class="clearfix margin-top3"></div>
 
 
 
-</div>
-
-<div class="col-lg-9 col-md-9 col-sm-7">
-<h2>Credit Union Owned Vehicles</h2>
-	
-	<p>From time to time, the Credit Union has vehicles for sale.  All vehicles are sold on an "as is" basis.  All sales are final.  Financing is available to eligible, qualified applicants.</p>
-	
-	
-	<div class="accordion_holder" data-anim-type="fade-in-up" data-anim-delay="100">
-          <div class="accordion_example1 smk_accordion acc_with_icon"> 
-            
-            
-            <div class="accordion_in three">
-              <div class="acc_head"><div class="acc_icon_expand"></div><div class="acc_icon_expand"></div>
-              <strong>2018 Mercedes-Benz GLE 350 - 17,013 mi - $43,000</strong></div>
-              <div class="acc_content"> 
-				<table class="table table-bordered table-striped" style="text-align: center;" role="presentation">
-	<thead>
-	
-	
-	</thead>
-
-<tbody>
-<tr>
-          <td width="41%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Photo </strong></td>
-          <td width="6%" align="center" bgcolor="#6C7E20"><div align="center"><strong style="color:#fff;">Year </strong></div></td>
-          <td width="14%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Make</strong></td>
-          <td width="20%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Model &amp; Description </strong></td>
-          <td width="9%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Mileage</strong></td>
-          <td width="10%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;"> Price</strong></td>
-        </tr>
-	<tr>
-          <td valign="top"><!--<img src="images/blog-img3.jpg">--><a href="images/repos/gle350-1.jpg" target="_blank"><img width="200" height="150" src="images/repos/gle350-1.jpg" alt="gle 350 benz image" /></a><br>
-            <a href="gle350.php">View additional images</a></td>
-          <td valign="top"><div><strong >2018</strong></div></td>
-          <td valign="top"><strong>Mercedes-Benz</strong></td>
-          <td valign="top"><strong>GLE 350 </strong></td>
-           <td valign="top"><strong>17,013 </strong></td>
-          <td valign="top"><strong>$43,000.00  </strong></td>
-        </tr>
-</tbody>
-</table>
-				 
-				</div>
-            </div>
-			  
-			 
-          </div>
-		
         </div>
-	
-	<!--<div class="accordion_holder" data-anim-type="fade-in-up" data-anim-delay="100">
+
+        <div class="col-lg-9 col-md-9 col-sm-7">
+          <h2>Credit Union Owned Vehicles</h2>
+
+          <p>From time to time, the Credit Union has vehicles for sale. All vehicles are sold on an "as is" basis. All sales are final. Financing is available to eligible, qualified applicants.</p>
+
+
+          <div class="accordion_holder" data-anim-type="fade-in-up" data-anim-delay="100">
+            <div class="accordion_example1 smk_accordion acc_with_icon">
+
+
+              <div class="accordion_in three">
+                <div class="acc_head">
+                  <div class="acc_icon_expand"></div>
+                  <div class="acc_icon_expand"></div>
+                  <strong>2018 Mercedes-Benz GLE 350 - 17,013 mi - $43,000</strong>
+                </div>
+                <div class="acc_content">
+                  <table class="table table-bordered table-striped" style="text-align: center;" role="presentation">
+                    <thead>
+
+
+                    </thead>
+
+                    <tbody>
+                      <tr>
+                        <td width="41%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Photo </strong></td>
+                        <td width="6%" align="center" bgcolor="#6C7E20">
+                          <div align="center"><strong style="color:#fff;">Year </strong></div>
+                        </td>
+                        <td width="14%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Make</strong></td>
+                        <td width="20%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Model &amp; Description </strong></td>
+                        <td width="9%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;">Mileage</strong></td>
+                        <td width="10%" align="center" bgcolor="#6C7E20"><strong style="color:#fff;"> Price</strong></td>
+                      </tr>
+                      <tr>
+                        <td valign="top">
+                          <!--<img src="images/blog-img3.jpg">--><a href="images/repos/gle350-1.jpg" target="_blank"><img width="200" height="150" src="images/repos/gle350-1.jpg" alt="gle 350 benz image" /></a><br>
+                          <a href="gle350.php">View additional images</a>
+                        </td>
+                        <td valign="top">
+                          <div><strong>2018</strong></div>
+                        </td>
+                        <td valign="top"><strong>Mercedes-Benz</strong></td>
+                        <td valign="top"><strong>GLE 350 </strong></td>
+                        <td valign="top"><strong>17,013 </strong></td>
+                        <td valign="top"><strong>$43,000.00 </strong></td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                </div>
+              </div>
+
+
+            </div>
+
+          </div>
+
+          <!--<div class="accordion_holder" data-anim-type="fade-in-up" data-anim-delay="100">
           <div class="accordion_example1 smk_accordion acc_with_icon"> 
             
             
@@ -188,63 +203,64 @@
           </div>
         </div>-->
 
-<p>For more information or to schedule a test drive, please <a href="contact.php">contact us</a>.</p>
-	
-
-<div class="clearfix margin-top3"></div>
-
-<div w3-include-html="side-nav-services-hide.php"></div>
-<div class="clearfix"></div>
+          <p>For more information or to schedule a test drive, please <a href="contact.php">contact us</a>.</p>
 
 
+          <div class="clearfix margin-top3"></div>
 
-</div>
+          <div w3-include-html="side-nav-services-hide.php"></div>
+          <div class="clearfix"></div>
 
-</div>
-</div>
 
-<div class="clearfix"></div>
-</div>
-<!--************THIS IS WHERE WE ARE INCLUDING THE FOOTER!!!! ************
+
+        </div>
+
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+  </div>
+  <!--************THIS IS WHERE WE ARE INCLUDING THE FOOTER!!!! ************
 	YOU CAN FIND IT IN FOOTER.php!!!!-->
-	
-	<?php include 'footer.php';?>
-	
-<!--*********FOOTER FOOTER FOOTER FOOTER FOOTER FOOTER **********-->
 
-<!-- ========== Js Files ========== --> 
+  <?php include 'footer.php'; ?>
 
-<script type="text/javascript" src="js/universal/jquery.js"></script>
-<script src="remodal/dist/remodal.js"></script> 
-	<script src="remodal/dist/remodal.js"></script>
-<script src="js/bootstrap/js/bootstrap.min.js" type="text/javascript"></script> 
-	<!--INCLUDE SCRIPT-->
-		<script src="js/custom.js" type="text/javascript"></script>
-		<script>
-		includeHTML();
-		</script>
-	<!--INCLUDE SCRIPT-->
-<script src="js/mainmenu/customeUI.js"></script>
-<script src="js/mainmenu/jquery.sticky.js"></script> 
-<script src="js/masterslider/masterslider.min.js"></script> 
+  <!--*********FOOTER FOOTER FOOTER FOOTER FOOTER FOOTER **********-->
 
-<script type="text/javascript" src="js/ytplayer/jquery.mb.YTPlayer.js"></script> 
-<script type="text/javascript" src="js/ytplayer/elementvideo-custom.js"></script> 
-<script type="text/javascript" src="js/ytplayer/play-pause-btn.js"></script>
-<script type="text/javascript" src="js/cubeportfolio/jquery.cubeportfolio.min.js"></script> 
-<script type="text/javascript" src="js/cubeportfolio/main.js"></script> 
-<script src="js/animations/js/animations.min.js" type="text/javascript"></script> 
-<script src="js/animations/js/appear.min.js" type="text/javascript"></script> 
-<script src="js/scrolltotop/totop.js"></script> 
-<script src="js/owl-carousel/owl.carousel.js"></script> 
-<script src="js/owl-carousel/custom.js"></script> 
-<script src="js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script> 
-<script type="text/javascript" src="js/tabs/smk-accordion.js"></script>
-<script type="text/javascript" src="js/tabs/custom.js"></script>
-<script src="js/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script> 
-<script src="js/style-swicher/style-swicher.js"></script> 
-<script src="js/style-swicher/custom.js"></script> 
-<script src="js/scripts/functions.js" type="text/javascript"></script>
+  <!-- ========== Js Files ========== -->
+
+  <script type="text/javascript" src="js/universal/jquery.js"></script>
+  <script src="remodal/dist/remodal.js"></script>
+  <script src="remodal/dist/remodal.js"></script>
+  <script src="js/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  <!--INCLUDE SCRIPT-->
+  <script src="js/custom.js" type="text/javascript"></script>
+  <script>
+    includeHTML();
+  </script>
+  <!--INCLUDE SCRIPT-->
+  <script src="js/mainmenu/customeUI.js"></script>
+  <script src="js/mainmenu/jquery.sticky.js"></script>
+  <script src="js/masterslider/masterslider.min.js"></script>
+
+  <script type="text/javascript" src="js/ytplayer/jquery.mb.YTPlayer.js"></script>
+  <script type="text/javascript" src="js/ytplayer/elementvideo-custom.js"></script>
+  <script type="text/javascript" src="js/ytplayer/play-pause-btn.js"></script>
+  <script type="text/javascript" src="js/cubeportfolio/jquery.cubeportfolio.min.js"></script>
+  <script type="text/javascript" src="js/cubeportfolio/main.js"></script>
+  <script src="js/animations/js/animations.min.js" type="text/javascript"></script>
+  <script src="js/animations/js/appear.min.js" type="text/javascript"></script>
+  <script src="js/scrolltotop/totop.js"></script>
+  <script src="js/owl-carousel/owl.carousel.js"></script>
+  <script src="js/owl-carousel/custom.js"></script>
+  <script src="js/tabs/assets/js/responsive-tabs.min.js" type="text/javascript"></script>
+  <script type="text/javascript" src="js/tabs/smk-accordion.js"></script>
+  <script type="text/javascript" src="js/tabs/custom.js"></script>
+  <script src="js/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+  <script src="js/style-swicher/style-swicher.js"></script>
+  <script src="js/style-swicher/custom.js"></script>
+  <script src="js/scripts/functions.js" type="text/javascript"></script>
 
 </body>
+
 </html>
