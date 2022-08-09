98b42bf92714c81fdfdbe541d2830c87c30e59bc
Dillon Ellis <dellis@fcul.com>
2022-08-09 09:21:26 -0400

    remove old files and add original index file

--- pdf/output/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// This is an example of config.php
-$dbhost = 'sql5c38b.carrierzone.com';
-$dbuser = 'ffcugaorg4843545';
-$dbpass = 'f@milyDB1';
-$dbname = 'phpmy1_ffcuga_org';
-?> 
--- pdf/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-mysql_connect('sql5c38b.carrierzone.com', 'ffcugaorg4843545', 'f@milyDB1') or die('Could not connect the database : Username or password incorrect');
-mysql_select_db('phpmy1_ffcuga_org') or die ('No database found');
-echo 'Database Connected successfully';
-?>
